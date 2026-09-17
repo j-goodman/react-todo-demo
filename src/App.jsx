cfc659412f615613c697220e8d28dc4b04a833cb
j-goodman <johncurtisgoodman@gmail.com>
2026-09-17 19:46:22 -0400

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+// import AddTask from './AddTask'
+// import TaskList from './TaskList'
+
+let nextId = 3
+const initialTasks = [
+  { id: 0, text: "Go buy pasta and eggs" },
+  { id: 1, text: "Walk the dog" },
+  { id: 2, text: "Clean the bathroom" }
+]
+
+function App() {
+  const [tasks, setTasks] = useState(initialTasks)
+
+  function handleAddTask(text) {
+    setTasks(
+      tasks.concat([{id: nextId++, text: text}])
+    )
+  }
+
+  function handleDeleteTask(id) {
+    setTasks(
+      tasks.filter(task => task.id !== id)
+    )
+  }
+
+  return (
+    <>
+      <h1>Task List!</h1>
+    </>
+  )
+}
+
+export default App
